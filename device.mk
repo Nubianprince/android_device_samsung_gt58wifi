89f734f061051a4310e1989789a660d4ace6d57f
Nubianprince <evereth@hotmail.com>
2019-03-31 18:40:17 -0400

    build.prop tweaks
    added tweaks to increase internet speed + quick power on

--- device.mk
+++ device.mk
@@ -39,12 +39,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=200 \
     telephony.lteOnCdmaDevice=0
     
-    
+# Better Internet Speed
+PRODUCT_PROPERTY_OVERRIDES += \
+net.tcp.buffersize.default=4096,87380,256960,4096, 16384,256960 \
+net.tcp.buffersize.wifi=4096,87380,256960,4096,163 84,256960 \
+net.tcp.buffersize.umts=4096,87380,256960,4096,163 84,256960 \
+net.tcp.buffersize.gprs=4096,87380,256960,4096,163 84,256960 \
+net.tcp.buffersize.edge=4096,87380,256960,4096,163 84,256960 \
+net.rmnet0.dns1=8.8.8.8 \
+net.rmnet0.dns2=8.8.4.4 \
+net.dns1=8.8.8.8 \
+net.dns2=8.8.4.4
+
+# Quick Power On
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.config.hw_quickpoweron=true
+
 BUILD_FINGERPRINT=samsung/gt58wifixx/gt58wifi:7.1.1/NMF26X/T350XXU1CQJ1:user/release-keys
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
   TARGET_DEVICE=gt58wifi \
   PRODUCT_NAME=gt58wifixx \
   PRIVATE_BUILD_DESC="gt58wifixx-user 7.1.1 NMF26X T350XXU1CQJ1 release-keys"
-
-
