637bb976f943e6afca45a3bae3c627504ab9849f
Nubianprince <evereth@hotmail.com>
2019-04-10 16:26:01 -0400

    removed config files back to using msm8916-common
    removed config files, updated msm8916-common to reflect correct audio
    path.

--- product/audio.mk
+++ product/audio.mk
@@ -1,7 +1,5 @@
 PRODUCT_COPY_FILES += \
-	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/mixer_paths.xml \
-	$(LOCAL_PATH)/configs/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
-	$(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
+	$(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/mixer_paths.xml
 
 # ANT+
 PRODUCT_PACKAGES += \
