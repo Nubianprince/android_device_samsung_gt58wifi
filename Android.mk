f297d7a39dd9ce04942cc3e4811b0870559d4fa8
Nubianprince <evereth@hotmail.com>
2019-05-15 20:37:54 -0400

    cleanup makefiles

--- Android.mk
+++ Android.mk
@@ -1,55 +1,23 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),gt58wifi)
 
-	include $(call all-makefiles-under,$(LOCAL_PATH))
-
-	include $(CLEAR_VARS)
-
-# Kiwi
-KIWI_IMAGES := \
-    kiwi.b00 kiwi.b01 kiwi.b02 kiwi.b03 kiwi.mdt
-
-KIWI_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(KIWI_IMAGES)))
-$(KIWI_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "KIWI firmware link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /firmware/image/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(KIWI_SYMLINKS)
-
-# Create links for audcal data files
-$(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wcd9306; \
-    ln -sf /data/misc/audio/wcd9320_anc.bin \
-    $(TARGET_OUT_ETC)/firmware/wcd9306/wcd9306_anc.bin; \
-    ln -sf /data/misc/audio/mbhc.bin \
-    $(TARGET_OUT_ETC)/firmware/wcd9306/wcd9306_mbhc.bin)
-
-
-
-RFS_MSM_ADSP_SYMLINKS := $(TARGET_OUT)/rfs/msm/adsp/
-$(RFS_MSM_ADSP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS MSM ADSP folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/tombstones/lpass $@/ramdumps
-	$(hide) ln -sf /persist/rfs/msm/adsp $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-
-RFS_MSM_MPSS_SYMLINKS := $(TARGET_OUT)/rfs/msm/mpss/
-$(RFS_MSM_MPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating RFS MSM MPSS folder structure: $@"
-	@rm -rf $@
-	@mkdir -p $(dir $@)/readonly
-	$(hide) ln -sf /data/tombstones/modem $@/ramdumps
-	$(hide) ln -sf /persist/rfs/msm/mpss $@/readwrite
-	$(hide) ln -sf /persist/rfs/shared $@/shared
-	$(hide) ln -sf /persist/hlos_rfs/shared $@/hlos
-	$(hide) ln -sf /firmware $@/readonly/firmware
-
-ALL_DEFAULT_INSTALLED_MODULES += $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
--- product/tweaks.mk
+++ product/tweaks.mk
@@ -8,21 +8,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.sf.lcd_density=160 \
 	telephony.lteOnCdmaDevice=0
 
-# Better Internet Speed
-PRODUCT_PROPERTY_OVERRIDES += \
-	net.tcp.buffersize.default=4096,87380,256960,4096,16384,256960 \
-	net.tcp.buffersize.wifi=4096,87380,256960,4096,163 84,256960
-
-# DNS	
-PRODUCT_PROPERTY_OVERRIDES += \
-	net.rmnet0.dns1=8.8.8.8 \
-	net.rmnet0.dns2=8.8.4.4 \
-	net.dns1=8.8.8.8 \
-	net.dns2=8.8.4.4
-
-# Fast Reboot
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.sys.purgeable_assets=1
 
 # Faster Scrolling
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -38,20 +23,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Quick Power On
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.hw_quickpoweron=true
-
-# Wireless Tweaks
-PRODUCT_PROPERTY_OVERRIDES += \
-	net.ipv4.ip_no_pmtu_disc=0 \
-	net.ipv4.route.flush=1 \
-	net.ipv4.tcp_ecn=0 \
-	net.ipv4.tcp_fack=1 \
-	net.ipv4.tcp_mem=187000 187000 187000 \
-	net.ipv4.tcp_moderate_rcvbuf=1 \
-	net.ipv4.tcp_no_metrics_save=1 \
-	net.ipv4.tcp_rfc1337=1 \
-	net.ipv4.tcp_rmem=4096 39000 187000 \
-	net.ipv4.tcp_sack=1 \
-	net.ipv4.tcp_timestamps=1 \
-	net.ipv4.tcp_window_scaling=1 \
-	net.ipv4.tcp_wmem=4096 39000 18700 \
-	wifi.supplicant_scan_interval=180
