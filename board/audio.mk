f89bdf87e71868e30c8d698e6c3f8a725ae5a391
Nubianprince <evereth@hotmail.com>
2021-01-23 23:38:26 -0500

    convert to twrp

--- board/audio.mk
+++
@@ -1,2 +0,0 @@
-# Low Latency
-AUDIO_USE_LL_AS_PRIMARY_OUTPUT := true
--- product/bluetooth.mk
+++
@@ -1,4 +0,0 @@
-# Bluetooth
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/bluetooth/iop_bt.db:$(TARGET_COPY_OUT_SYSTEM)/etc/bluetooth/iop_bt.db \
-    $(LOCAL_PATH)/configs/bluetooth/iop_device_list.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/bluetooth/iop_device_list.conf
